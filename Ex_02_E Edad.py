8968c15d889a2cf30b56fdbcf00b8cc7b138cae6
ElProfeAnwar <150026331+ElProfeAnwar@users.noreply.github.com>
2023-11-06 10:12:33 -0300

    Update Ex_02_E Edad.py

--- Ex_02_E Edad.py
+++ Ex_02_E Edad.py
@@ -3,3 +3,22 @@ anio_actual = int(input("Ingrese año actual: "))
 anio_nac = int(input("Ingrese año nacimiento: "))
 edad = anio_actual-anio_nac
 print ("Usted tiene ", edad)
+#Con IF ELIF
+
+if edad >= 18 and edad <65:
+    print ("Mayor de edad")
+elif edad >=65:
+    print("Tercera edad")
+else:
+    print("Menor de edad")
+
+#Con si anidado
+""" 
+if edad >= 65:
+    print("Tay awelito")
+else:
+    if edad >= 18:
+        print("y es mayor de edad")
+    else:
+        print("Erís terrile pollo")
+"""    

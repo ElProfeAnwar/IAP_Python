7cd4ca1c1166f2363db20f043efaf5e0d858b679
ElProfeAnwar <150026331+ElProfeAnwar@users.noreply.github.com>
2023-11-06 09:19:47 -0300

    Add files via upload

---
+++ Ex_01_1.py
@@ -0,0 +1,7 @@
+
+#Definiendo las variables
+
+cant = int(input("Ingrese la cantidad invertida: "))
+anios = int(input("Ingrese la cantidad de años: "))
+
+print("La cantidad final es: ", cant*anios)
